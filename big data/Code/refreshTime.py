0176cef902fa6579028f5a0171b12fdd118c06e4
franckatte <kassi.atteaka@epfl.ch>
2020-12-17 18:43:10 +0100

    ajout word et correction refresh time function

--- big data/Code/refreshTime.py
+++ big data/Code/refreshTime.py
@@ -9,22 +9,34 @@ import numpy as np
 
 def test_date(dates):
     '''test wether one of the dates list is empty'''
-    try:
-        return np.apply_along_axis(lambda x: len(x) == 0 ,1, dates.reshape((-1,1))).sum()
-    except ValueError:
-        return 1
+    
+    a = [len(date) for date in dates]
+    
+    return 0 in a
 
 def refresh_time(dfs):
-    '''return list of refresh times'''
+    '''
+        return list of refresh times 
+        parameters
+        ------------
+        dfs(list): list of dataframes
+        
+        Return
+        ------------
+        tau(numpy array): contain refresh times
+        
+    '''
     dates = np.array([df.loc[~df.index.duplicated(keep='last')].index for df in dfs])
     tau = []
     
-    while test_date(dates)< 1:
+    while test_date(dates) == False:
         
         # append refresh date
         tau.append(max([min(date) for date in dates]))
 
         #update dates
         dates = np.array([date[np.where(date > tau[-1])[0]] for date in dates ])
-    
+        
+      
+        
     return tau
