9c1a6513df873fa4037d15825dbb85e9b1821d55
coco369444 <c.bourdeix@orange.fr>
2020-12-19 18:08:07 +0100

    Update Clustering.py

--- big data/Code/Clustering.py
+++ big data/Code/Clustering.py
@@ -4,6 +4,8 @@ Created on Sat Dec 19 15:59:19 2020
 
 @author: corentin
 """
+
+#pip install python-louvain
 import numpy as np
 import pandas as pd
 import networkx as nx
@@ -22,6 +24,17 @@ def zero_diag_matrix(M):
 
 
 def creat_graph(corr_matrix):
+    '''
+        return DataFrame of synchronise data
+        ------------
+        corr_matrix(DataFrame): correlation matrix of the market DataFrame
+        
+        Return
+        ------------
+        G(networkx Graph): graph unidirect with the market and the weight are the correlation
+        
+    '''
+    
     #input : correlation matrix of the market DataFrame
     #
     #return : the graph with weight of each link
@@ -33,6 +46,23 @@ def creat_graph(corr_matrix):
     G = nx.relabel_nodes(G, label_mapping)
     
     return G
+
+def get_clusters(G):
+    '''
+        return DataFrame with cluster_label
+        ------------
+        G(networkx Graph): graph unidirect with the market and the weight are the correlation
+        
+        Return
+        ------------
+        Label(DataFrame): columns : market / row : label
+        
+    '''
+    partition = community_louvain.best_partition(G)
+    
+    return partition
+
+
     
     
     
