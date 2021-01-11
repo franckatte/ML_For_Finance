5fb1f9fd8d7bdc70f39ecf9b7fe33ce4268c291e
coco369444 <c.bourdeix@orange.fr>
2021-01-11 23:28:20 +0100

    Update data_cleaning.py

--- big data/Code/data_cleaning.py
+++ big data/Code/data_cleaning.py
@@ -14,16 +14,16 @@ dask.config.set(scheduler="processes")
 
 #We create a function to load the trades of one market on one day
 @dask.delayed
-def load_trade(market_name,date,folder_path,tz_exchange = "America/New_York",
+def load_trade(stock_name,date,folder_path,tz_exchange = "America/New_York",
                only_regular_trading_hours = True,open_time = "09:30:00",close_time = "16:00:00",is_compressed = False):
     
     stock_path = 'extraction/TRTH/raw/equities/US/trade/'
     #the way top extract the data is different depending if the data are compressed or not
     if is_compressed == False:
-        path_name = folder_path + stock_path + market_name + '/' + str(date)[:10] + '-' + market_name + '-trade.csv'
+        path_name = folder_path + stock_path + stock_name + '/' + str(date)[:10] + '-' + stock_name + '-trade.csv'
         DF = pd.read_csv(path_name)[['xltime','trade-price']]
     else:
-        path_name = folder_path + stock_path + market_name + '/' + str(date)[:10] + '-' + market_name + '-trade.csv.gz'
+        path_name = folder_path + stock_path + stock_name + '/' + str(date)[:10] + '-' + stock_name + '-trade.csv.gz'
         DF = pd.read_csv(path_name, compression = 'gzip')[['xltime','trade-price']]
         
     
@@ -39,13 +39,13 @@ def load_trade(market_name,date,folder_path,tz_exchange = "America/New_York",
     if only_regular_trading_hours:
         DF = DF.between_time(open_time,close_time)    
     
-    DF = DF.rename(columns = {'trade-price': market_name})
+    DF = DF.rename(columns = {'trade-price': stock_name})
     return DF
 
 #This function use dask to do 'load_trade' for all markets at the same time
-def impor_data(market_name,day,path):
+def impor_data(stock_name,day,path):
     #We creat a list to import the data
-    sortie = [load_trade(market,day,path,is_compressed=True) for market in market_name]
+    sortie = [load_trade(stock,day,path,is_compressed=True) for stock in stock_name]
     return dask.compute(sortie)[0]
 
 
