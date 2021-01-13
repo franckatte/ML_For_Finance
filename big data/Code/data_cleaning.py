44e715d48e68637ac26b3f7b313883986c7a29fd
coco369444 <c.bourdeix@orange.fr>
2021-01-13 20:02:43 +0100

    Update data_cleaning.py

--- big data/Code/data_cleaning.py
+++ big data/Code/data_cleaning.py
@@ -12,13 +12,13 @@ dask.config.set(scheduler="processes")
 
 
 
-#We create a function to load the trades of one market on one day
+#We create a function to load the trades of one asset for a given day day
 @dask.delayed
 def load_trade(stock_name,date,folder_path,tz_exchange = "America/New_York",
                only_regular_trading_hours = True,open_time = "09:30:00",close_time = "16:00:00",is_compressed = False):
     
     stock_path = 'extraction/TRTH/raw/equities/US/trade/'
-    #the way top extract the data is different depending if the data are compressed or not
+    #the way top extract the data is different depending if the data are zipped or not
     if is_compressed == False:
         path_name = folder_path + stock_path + stock_name + '/' + str(date)[:10] + '-' + stock_name + '-trade.csv'
         DF = pd.read_csv(path_name)[['xltime','trade-price']]
@@ -42,7 +42,7 @@ def load_trade(stock_name,date,folder_path,tz_exchange = "America/New_York",
     DF = DF.rename(columns = {'trade-price': stock_name})
     return DF
 
-#This function use dask to do 'load_trade' for all markets at the same time
+#This function use dask to do 'load_trade' for all assets at the same time
 def impor_data(stock_name,day,path):
     #We creat a list to import the data
     sortie = [load_trade(stock,day,path,is_compressed=True) for stock in stock_name]
